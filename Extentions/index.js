315520f16d2b1f50fea8df93fb4c915f7a522f0a
Trugtie <nhamtrungtien2013@gmail.com>
2021-10-04 20:38:40 +0700

    Deploy Extention

--- Extentions/index.js
+++ Extentions/index.js
@@ -10,11 +10,58 @@
 
 // console.log("Total cost: "+fullPrice+". It will arrive in "+ shippingTime)
 
+// const body=document.querySelector("body");
+
+// body.innerHTML="<button onclick='buy()'> Buy! </button>";
+
+// function buy() {
+//     body.innerHTML +="<p>Thanks you for buying!</p>"
+// }
+
+const recipient = "James"
+const sender = "Tien"
+
+const email=`Hey ${recipient}!
+How is it going?
+Cheers ${sender}`
+
+console.log(email)
+
 let myLeads=[];
 const button = document.querySelector("#input-btn");
 const inputEl=document.querySelector("#input-el");
+const ulEl=document.querySelector("#ul-el");
+
 
 button.addEventListener("click",function (){
     myLeads.push(inputEl.value);
-    console.log(myLeads);
+    inputEl.value="";
+    renderLeads();
 })
+
+function renderLeads(){
+    let listItems="";
+    for (let i = 0 ; i < myLeads.length ; i++) {
+        // listItems+="<li><a href='"+ myLeads[i] +"' target='_blank'>"+ myLeads[i] + "</a></li>"
+        listItems += `
+        <li>
+            <a href="${myLeads[i]}" target="_blank">
+             ${myLeads[i]}
+            </a>
+        </li>
+        `
+        // const li = document.createElement("li");
+        // li.textContent=myLeads[i];
+        // ulEl.append(li);
+    }
+    
+    ulEl.innerHTML=listItems;
+}
+
+
+
+
+
+
+
+
