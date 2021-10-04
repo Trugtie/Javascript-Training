99ab74e12f9b79be617faa99ae353a263139b40a
Trugtie <nhamtrungtien2013@gmail.com>
2021-10-04 23:06:45 +0700

    Complete Extentions Project

--- Extentions/index.js
+++ Extentions/index.js
@@ -18,50 +18,110 @@
 //     body.innerHTML +="<p>Thanks you for buying!</p>"
 // }
 
-const recipient = "James"
-const sender = "Tien"
+// const recipient = "James"
+// const sender = "Tien"
 
-const email=`Hey ${recipient}!
-How is it going?
-Cheers ${sender}`
+// const email=`Hey ${recipient}!
+// How is it going?
+// Cheers ${sender}`
 
-console.log(email)
+// console.log(email)
 
-let myLeads=[];
+// localStorage.setItem("myName","Tien")
+// console.log(localStorage.getItem("myName"))
+// localStorage.clear()
+
+//6 falsy values
+//0
+// ""
+// null
+// undefined
+// false
+// NaN
+
+// console.log(Boolean("")) // false
+// console.log(Boolean("0"))//true
+// console.log(Boolean(100))//true
+// console.log(Boolean(null))//false
+// console.log(Boolean([0]))//true
+// console.log(Boolean(-0))//false
+
+// function greetUser(welcome,name,emoji){
+//     console.log(`${welcome}, ${name} ${emoji}`)
+// }
+
+// greetUser("Hello","Peter",":D")
+// greetUser("Hi","Clay","xD")
+// greetUser("Welcome","Han","!")
+
+// function add(number1,number2){
+// return number1 + number2;
+// }
+
+// console.log(add(1,2));
+// function getFirst(arr){
+//     return arr[0];
+// }
+
+// console.log(getFirst([5,8,7,3]))
+
+// let myLeads=[];
+
+// myLeads=JSON.parse(myLeads);
+// myLeads.push("www.facebook.com");
+// myLeads=JSON.stringify(myLeads);
+// console.log(typeof myLeads);
+
 const button = document.querySelector("#input-btn");
-const inputEl=document.querySelector("#input-el");
-const ulEl=document.querySelector("#ul-el");
+const inputEl = document.querySelector("#input-el");
+const ulEl = document.querySelector("#ul-el");
+const deleteBtn = document.querySelector("#delete-btn");
+const tabBtn = document.querySelector("#tab-btn");
 
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
+// console.log(leadsFromLocalStorage)
 
-button.addEventListener("click",function (){
-    myLeads.push(inputEl.value);
-    inputEl.value="";
-    renderLeads();
-})
-
-function renderLeads(){
-    let listItems="";
-    for (let i = 0 ; i < myLeads.length ; i++) {
-        // listItems+="<li><a href='"+ myLeads[i] +"' target='_blank'>"+ myLeads[i] + "</a></li>"
-        listItems += `
-        <li>
-            <a href="${myLeads[i]}" target="_blank">
-             ${myLeads[i]}
-            </a>
-        </li>
-        `
-        // const li = document.createElement("li");
-        // li.textContent=myLeads[i];
-        // ulEl.append(li);
-    }
-    
-    ulEl.innerHTML=listItems;
+if (leadsFromLocalStorage) {
+  myLeads = leadsFromLocalStorage;
+  render(myLeads);
 }
 
+tabBtn.addEventListener("click", function (tabs) {
+  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    myLeads.push(tabs[0].url);
+    localStorage.setItem("myLeads", JSON.stringify(myLeads));
+    render(myLeads);
+  });
+});
 
+function render(leads) {
+  let listItems = "";
+  for (let i = 0; i < leads.length; i++) {
+    // listItems+="<li><a href='"+ myLeads[i] +"' target='_blank'>"+ myLeads[i] + "</a></li>"
+    listItems += `
+        <li>
+            <a href="${leads[i]}" target="_blank">
+             ${leads[i]}
+            </a>
+        </li>
+        `;
+    // const li = document.createElement("li");
+    // li.textContent=myLeads[i];
+    // ulEl.append(li);
+  }
 
+  ulEl.innerHTML = listItems;
+}
 
+deleteBtn.addEventListener("dblclick", function () {
+  localStorage.clear();
+  myLeads = [];
+  render(myLeads);
+});
 
-
-
-
+button.addEventListener("click", function () {
+  myLeads.push(inputEl.value);
+  inputEl.value = "";
+  localStorage.setItem("myLeads", JSON.stringify(myLeads));
+  render(myLeads);
+});
